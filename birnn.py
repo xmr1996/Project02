d0d2f15e9df758dae7ac28ae59f4c0d6f12e0911
nmscannell <scannell@uwm.edu>
2019-12-08 13:43:43 -0600

    example rnn on imdb from tutorial/github

--- birnn.py
+++ birnn.py
@@ -2,60 +2,36 @@
 #https://github.com/balag59/imdb-sentiment-bidirectional-LSTM/blob/master/imdb_bilstm_train.py
 
 from tensorflow.python.keras.datasets import imdb
+from tensorflow.python.keras.preprocessing import sequence
+from keras import Sequential
+from keras.layers import Embedding, LSTM, Dense, Dropout, Bidirectional
 import numpy as np
 
-(train_data, train_label), (test_data, test_labels) = imdb.load_data(num_words=10000)
-
-print("length of train_data and test_data: ", len(train_data), len(test_data))
-print(train_data[0])
-print(train_label[0])
-print(max([max(sequence) for sequence in train_data]))
-
-word_index = imdb.get_word_index()
-
-reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
-decoded_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
-print("review in train_data[0]: ", decoded_review)
-
-def vectorize_sequences(sequences, dimension=10000):
-    results = np.zeros((len(sequences), dimension))
-    for i, each_sentence in enumerate(sequences):
-        results[i, each_sentence] = 1
-    return results
-
-x_train = vectorize_sequences(train_data)
-x_test = vectorize_sequences(test_data)
-
-y_train = np.asarray(train_label).astype('float32')
-y_test = np.asarray(test_labels).astype('float32')
-
-x_train = np.concatenate((x_train, x_test[:15000]))
-x_test = x_test[15000:]
-y_train = np.concatenate((y_train, test_labels[:15000]))
-y_test = y_test[15000:]
-
-
-from tensorflow.python.keras import models, layers
-
-#maybe change?
 max_features = 10000
+max_length = 500
 
-network = models.Sequential()
-network.add(layers.Embedding(max_features, 128))
-network.add(layers.Bidirectional(layers.LSTM(64, input_shape=(10000,))))
-network.add(layers.Dropout(0.5))
-network.add(layers.Bidirectional(layers.LSTM(64)))
-network.add(layers.Dropout(0.5))
-network.add(layers.Dense(20, activation='relu'))
-network.add(layers.Dropout(0.05))
-network.add(layers.Dense(1, activation='sigmoid'))
+(train_data, train_label), (test_data, test_labels) = imdb.load_data(num_words=max_features)
 
-network.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-x_val = x_train[:10000]
-partial_x_training = x_train[10000:]
-y_val = y_train[:10000]
-partial_y_training = y_train[10000:]
-history = network.fit(partial_x_training, partial_y_training, epochs=20, batch_size=256, validation_data=(x_val, y_val))
+train_data = sequence.pad_sequences(train_data, maxlen=max_length)
+test_data = sequence.pad_sequences(test_data, maxlen=max_length)
+
+train_data = np.concatenate((train_data, test_data[:15000]))
+test_data = test_data[15000:]
+train_label = np.concatenate((train_label, test_labels[:15000]))
+test_labels = test_labels[15000:]
+
+embedding_size = 32
+network = Sequential()
+network.add(Embedding(max_features, embedding_size, input_length=max_length))
+network.add(Bidirectional(LSTM(64)))
+network.add(Dropout(0.2))
+network.add(Bidirectional(LSTM(64)))
+network.add(Dropout(0.2))
+network.add(Dense(20, activation='relu'))
+network.add(Dense(1, activation='sigmoid'))
+
+network.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+history = network.fit(train_data, train_label, epochs=15, batch_size=64, validation_split=0.2)
 
 import matplotlib.pyplot as plt
 history_dict = history.history
@@ -79,3 +55,5 @@ plt.title('Training and validation accuracy')
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.show()
+
+print(network.evaluate(test_data, test_labels))
--- rnn.py
+++ rnn.py
@@ -3,60 +3,39 @@
 #https://towardsdatascience.com/light-on-math-ml-attention-with-keras-dc8dbc1fad39
 #https://github.com/thushv89/attention_keras
 from tensorflow.python.keras.datasets import imdb
+from tensorflow.python.keras.preprocessing import sequence
+from keras import Sequential
+from keras.layers import Embedding, LSTM, Dense, Dropout
 #from attention_keras.layers.attention import AttentionLayer
 import numpy as np
 
-(train_data, train_label), (test_data, test_labels) = imdb.load_data(num_words=10000)
+max_features = 10000
+max_length = 500
 
-print("length of train_data and test_data: ", len(train_data), len(test_data))
-print(train_data[0])
-print(train_label[0])
-print(max([max(sequence) for sequence in train_data]))
+(train_data, train_label), (test_data, test_labels) = imdb.load_data(num_words=max_features)
 
-word_index = imdb.get_word_index()
+train_data = sequence.pad_sequences(train_data, maxlen=max_length)
+test_data = sequence.pad_sequences(test_data, maxlen=max_length)
 
-reverse_word_index = dict([(value, key) for (key, value) in word_index.items()])
-decoded_review = ' '.join([reverse_word_index.get(i - 3, '?') for i in train_data[0]])
-print("review in train_data[0]: ", decoded_review)
-
-def vectorize_sequences(sequences, dimension=10000):
-    results = np.zeros((len(sequences), dimension))
-    for i, each_sentence in enumerate(sequences):
-        results[i, each_sentence] = 1
-    return results
-
-x_train = vectorize_sequences(train_data)
-x_test = vectorize_sequences(test_data)
-print(x_train[0].ndim)
-print(x_test.ndim)
-
-y_train = np.asarray(train_label).astype('float32')
-y_test = np.asarray(test_labels).astype('float32')
-index = 2
-print(x_train[index])
-print(y_train[index])
-
-from keras import Sequential
-from keras.layers import Embedding, LSTM, Dense, Dropout
+train_data = np.concatenate((train_data, test_data[:15000]))
+test_data = test_data[15000:]
+train_label = np.concatenate((train_label, test_labels[:15000]))
+test_labels = test_labels[15000:]
 
 embedding_size = 32
 model = Sequential()
-model.add(Embedding(10000, embedding_size, input_length=500))
+model.add(Embedding(max_features, embedding_size, input_length=max_length))
 model.add(LSTM(128))
-model.add(Dropout(0.4))
+model.add(Dropout(0.2))
 model.add(LSTM(128))
-model.add(Dropout(0.4))
+model.add(Dropout(0.2))
 # need attention
-model.add(Dense(256), activation='relu')
+model.add(Dense(64), activation='relu')
 model.add(Dropout(0.5))
-model.add(Dense(1, activation='softmax'))
+model.add(Dense(1, activation='sigmoid'))
 
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-x_val = x_train[:10000]
-partial_x_training = x_train[10000:]
-y_val = y_train[:10000]
-partial_y_training = y_train[10000:]
-history = model.fit(partial_x_training, partial_y_training, epochs=20, batch_size=512, validation_data=(x_val, y_val))
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(train_data, train_label, epochs=15, batch_size=64, validation_split=0.2)
 
 import matplotlib.pyplot as plt
 history_dict = history.history
@@ -80,3 +59,5 @@ plt.title('Training and validation accuracy')
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.show()
+
+print(model.evaluate(test_data, test_labels))
